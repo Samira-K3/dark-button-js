c4075b383a42d2e0b4251c626e9c127eedf7b32c
Samira-K3 <you@example.comsamira.kabir63@gmail.com>
2024-02-11 21:48:43 +0600

    first commit

---
+++ custom.js
@@ -0,0 +1,8 @@
+function dark() {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+}
+function usual() {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+}
